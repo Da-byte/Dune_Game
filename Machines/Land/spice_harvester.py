41731d84624e46edb78ad2b12d1d1fbb3d653d6c
David-Ovidiu <naeovidiu@yahoo.com>
2022-02-13 22:16:45 +0000

    first commit

---
+++ Machines/Land/spice_harvester.py
@@ -0,0 +1,17 @@
+
+class SpiceHarvester:
+
+    # The spice harvester harvests spice from a spice hoard
+
+    def __init__(self, size, colour, banner, man_power, spice_capacity, speed, armour, attack):
+        self.size = size
+        self.colour = colour
+        self.banner = banner
+        self.man_power = man_power
+        self.spice_capacity = spice_capacity
+        self.speed = speed
+        self.armour = armour
+        self.attack = attack
+
+    #def harvest(self, time, hoard_size):
+
